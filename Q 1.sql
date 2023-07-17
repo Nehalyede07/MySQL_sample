baa939d246d7993ae3325eaee5064bce1b79fd4b
Nehalyede07 <139756615+Nehalyede07@users.noreply.github.com>
2023-07-18 00:34:20 +0530

    Q 1.sql

--- Q 1.sql
+++ Q 1.sql
@@ -1,3 +1,5 @@
+#Q.1 How many transactions were completed during each marketing campaign?
+
 select campaign_name,count(transaction_id) as no_of_transaction
 from transactions
 Inner join marketing_campaigns on transactions.purchase_date between start_date and end_date
